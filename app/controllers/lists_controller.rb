1c91783055e0b4bbc1d150f2184582879363c001
Linda Luu and Safiy Cham <safiy@>
2017-11-13 11:27:33 -0800

    add edit to list.

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -24,6 +24,20 @@ class ListsController < ApplicationController
     end
   end
 
+  def edit
+    @list = List.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @list = List.find(params[:id])
+    if @list.update(list_params)
+      redirect_to lists_path
+    else
+      render :edit
+    end
+  end
+
 private
   def list_params
     params.require(:list).permit(:name)
